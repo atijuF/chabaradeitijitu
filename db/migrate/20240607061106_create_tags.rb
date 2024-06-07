1d8ce0e832a3c68ad584f220318ffc40ded7dc93
atijuF <162565598+atijuF@users.noreply.github.com>
2024-06-07 06:44:50 +0000

    mgファイル作成

---
+++ db/migrate/20240607061106_create_tags.rb
@@ -0,0 +1,10 @@
+class CreateTags < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tags do |t|
+      t.references :post, null: false, foreign_key: true
+      t.string :tag
+
+      t.timestamps
+    end
+  end
+end
