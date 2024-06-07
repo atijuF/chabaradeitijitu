3aba6ef04f420b9456fa6947a4cc8984d74e88ea
atijuF <162565598+atijuF@users.noreply.github.com>
2024-06-07 11:01:30 +0000

    top、about作成、ユーザー新規登録作成中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
+  
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+  
+  def applicaton
+    @user = User.find(params[:id])
+    #@posts = @user.posts.page(params[:page])
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+  end
 end
--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,7 +1,10 @@
 class Public::RegistrationsController < ApplicationController
   def new
+    @user = User.new
+    resource = @user
+    #resourceが未定義になっている、理由不明
   end
-
   def create
   end
+  
 end
