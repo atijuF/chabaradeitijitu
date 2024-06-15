de273dba599f6e93c5a55f12631db0c142bd9519
atijuF <162565598+atijuF@users.noreply.github.com>
2024-06-15 04:25:02 +0000

    mgファイル修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_09_034623) do
+ActiveRecord::Schema.define(version: 2024_06_15_041837) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -71,14 +71,22 @@ ActiveRecord::Schema.define(version: 2024_06_09_034623) do
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
+  create_table "post_tags", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id", "tag_id"], name: "index_post_tags_on_post_id_and_tag_id", unique: true
+    t.index ["post_id"], name: "index_post_tags_on_post_id"
+    t.index ["tag_id"], name: "index_post_tags_on_tag_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
-    t.integer "tag_id"
     t.string "title", null: false
     t.text "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["tag_id"], name: "index_posts_on_tag_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -93,11 +101,9 @@ ActiveRecord::Schema.define(version: 2024_06_09_034623) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.integer "post_id", null: false
     t.string "tag", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_tags_on_post_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -120,7 +126,5 @@ ActiveRecord::Schema.define(version: 2024_06_09_034623) do
   add_foreign_key "comments", "users"
   add_foreign_key "favorites", "posts"
   add_foreign_key "favorites", "users"
-  add_foreign_key "posts", "tags"
   add_foreign_key "posts", "users"
-  add_foreign_key "tags", "posts"
 end
