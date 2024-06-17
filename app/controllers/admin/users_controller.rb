9e60996665a1eee0eafb9bd3f6d6e7516c122448
atijuF <162565598+atijuF@users.noreply.github.com>
2024-06-17 03:48:58 +0000

    Adminユーザー機能追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,17 +1,31 @@
 class Admin::UsersController < ApplicationController
   before_action :authenticate_admin!
+  
   def index
+    @users = User.page(params[:page])
   end
-
+  
   def show
+    @user = User.find(params[:id])
   end
-
+  
   def edit
+    @user = User.find(params[:id])
   end
-
+  
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to admin_user_path(@user)
+    else
+      render :edit
+    end
   end
-
-  def destroy
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :profile_image, :introduction, :is_active)
   end
 end
