931dee50346237111f87e4c229aac9827c20c114
atijuF <162565598+atijuF@users.noreply.github.com>
2024-06-24 08:00:45 +0000

    レイアウト修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,9 +4,9 @@ class SearchesController < ApplicationController
     @word = params[:word]
     @search = params[:search]
     if @range == "User"
-      @users = User.looks(params[:search], params[:word])
+      @users = User.looks(params[:search], params[:word]).page(params[:page])
     elsif @range == "Post"
-      @posts = Post.looks(params[:search], params[:word])
+      @posts = Post.looks(params[:search], params[:word]).page(params[:page])
     #elsif @range == "Tag"
     #  @posts = Post.joins(:tags).where(tags: { name: params[:word] }).distinct
     end
