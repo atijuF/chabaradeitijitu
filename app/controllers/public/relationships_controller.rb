8bfc36380b4ebe3bb5764d196b76096d3c0d8435
atijuF <162565598+atijuF@users.noreply.github.com>
2024-07-07 10:00:52 +0000

    public側リファクタリング

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,25 +1,28 @@
 class Public::RelationshipsController < ApplicationController
   before_action :authenticate_user!
-  # フォローするとき
+  before_action :set_user, only: [:create, :destroy, :followings, :followers]
+
   def create
-    @user = User.find(params[:user_id])
-    current_user.follow(params[:user_id])
-    redirect_to request.referer
+    current_user.follow(@user)
+    redirect_back(fallback_location: root_path)
   end
-  # フォロー外すとき
+
   def destroy
-    @user = User.find(params[:user_id])
     current_user.unfollow(@user)
-    redirect_to request.referer
+    redirect_back(fallback_location: root_path)
   end
-  # フォロー一覧
+
   def followings
-    @user = User.find(params[:user_id])
     @users = @user.followings.page(params[:page]).per(10)
   end
-  # フォロワー一覧
+
   def followers
-    @user = User.find(params[:user_id])
     @users = @user.followers.page(params[:page]).per(10)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,29 +1,21 @@
 class Public::UsersController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update, :destroy]
+  before_action :set_user, only: [:edit, :update, :destroy, :show]
   before_action :is_matching_login_user, only: [:edit, :update, :destroy]
 
   def edit
-    @user = User.find(params[:id])
-    is_matching_login_user
   end
 
   def show
-    @user = User.find(params[:id])
     @posts = @user.posts.where(status: 0).page(params[:page])
     @new_post = Post.new
-    @favorite_posts = @user.favorite_posts
-    if user_signed_in?
-      @favorite_posts = current_user.favorite_posts.page(params[:favorite_posts_page]).per(10)
-    else
-      @favorite_posts = @user.favorite_posts.page(params[:favorite_posts_page]).per(10)
-    end
+    @favorite_posts = user_signed_in? ? current_user.favorite_posts.page(params[:favorite_posts_page]).per(10) : @user.favorite_posts.page(params[:favorite_posts_page]).per(10)
+    
     # ステータスが1の投稿の確認とメッセージ表示
     @inappropriate_post_message = "この投稿は不適切なため管理者によって削除されました。" if @user.posts.exists?(status: 1)
   end
 
   def update
-    is_matching_login_user
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "successfully"
       redirect_to user_path(@user.id)
@@ -33,7 +25,6 @@ class Public::UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     if @user.destroy
       sign_out @user  # ログアウト処理
       flash[:notice] = "退会しました"
@@ -46,14 +37,15 @@ class Public::UsersController < ApplicationController
   
   private
 
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
   def is_matching_login_user
-    user = User.find(params[:id])
-    unless user.id == current_user.id
-      redirect_to user_path(current_user.id)
-    end
+    redirect_to user_path(current_user.id) unless @user.id == current_user.id
   end
 end
